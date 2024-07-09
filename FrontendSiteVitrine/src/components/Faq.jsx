8ff6650e4ba9f26acc53f1aa3a2fff3fbcc90014
HarenaRaelison <123414376+HarenaRaelison@users.noreply.github.com>
2024-07-09 15:14:10 +0300

    ajustement Link

--- FrontendSiteVitrine/src/components/Faq.jsx
+++ FrontendSiteVitrine/src/components/Faq.jsx
@@ -33,7 +33,8 @@ const Faq = () => {
                 </div>
                 <div className="text-center mt-8">
                     <Link to="/contact-form" className="inline-block px-8 py-3 bg-blue-600 text-white font-bold rounded shadow-lg hover:bg-blue-700 transition duration-300">
-                        faire les démarche DA
+                        faire les démarche Dessin animé
+                        
                     </Link>
                 </div>
             </div>
