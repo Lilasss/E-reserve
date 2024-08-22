a5322091b11a4c0c5efc596d1ba8d7af4f7c8fef
Lilasss <razafindramboahantasoa@gmail.com>
2024-08-22 12:17:21 +0300

    22

--- FrontendSiteVitrine/src/pages/PageUser/SiteVitrineUser/BarNav.jsx
+++ FrontendSiteVitrine/src/pages/PageUser/SiteVitrineUser/BarNav.jsx
@@ -33,84 +33,87 @@ const BarNav = () => {
 
     return (
         <>
-            <header className="bg-gradient-to-r from-[#0A5DA6] to-[#0A5DA6] p-5 text-white fixed top-0 w-full z-50 shadow-lg">
-                <div className="container mx-auto flex justify-between items-center">
-                    <h1 className="flex items-center cursor-pointer" onClick={() => navigate('/home')}>
-                        <img src={logoImage} alt="E-Reserve" className="h-12 w-auto" />
-                    </h1>
-                    <nav>
-                        <ul className="flex space-x-10">
-                            <li>
-                                <Link to="/home" className="hover:text-yellow-300 transition">Accueil</Link>
-                            </li>
-                            <li>
-                                <Link to="/home" className="hover:text-yellow-300 transition">Devenir administrateur</Link>
-                            </li>
-                            <li className="relative group">
-                                <span className="hover:text-yellow-300 transition cursor-pointer flex items-center">
-                                    Services
-                                    <FaChevronDown className="ml-2 text-sm" style={{ fontWeight: 'normal' }} />
-                                </span>
-                                <ul className="absolute left-0 mt-2 w-39 bg-white text-blue-800 shadow-lg rounded-lg opacity-0 group-hover:opacity-100 group-hover:translate-y-1 transition-all duration-300 ease-in-out transform scale-95 group-hover:scale-100">
-                                    <li className="hover:bg-blue-600 hover:text-white transition rounded-t-lg">
-                                        <button onClick={() => scrollToSection('carousel')} className="block px-4 py-2 text-left w-full">Événement</button>
-                                    </li>
-                                    <li className="hover:bg-blue-600 hover:text-white transition">
-                                        <button onClick={() => scrollToSection('chambre')} className="block px-4 py-2 text-left w-full">Chambre</button>
-                                    </li>
-                                    <li className="hover:bg-blue-600 hover:text-white transition rounded-b-lg">
-                                        <button onClick={() => scrollToSection('reserve')} className="block px-4 py-2 text-left w-full">Transport</button>
-                                    </li>
-                                </ul>
-                            </li>
-                            <li>
-                                <span
-                                    onClick={() => navigate('/login')}
-                                    className="bg-yellow-400 text-blue-800 font-semibold py-3 px-8 rounded-full hover:bg-blue-800 hover:text-white transition duration-300 cursor-pointer"
-                                >
-                                    Se connecter
-                                </span>
-                            </li>
-                        </ul>
-                    </nav>
+           {/* style={{ fontFamily: 'Poppins, sans-serif' }} */}
+            <div>
+                <header className="bg-gradient-to-r from-[#0A5DA6] to-[#0A5DA6] p-5 text-white fixed top-0 w-full z-50 shadow-lg">
+                    <div className="container mx-auto flex justify-between items-center">
+                        <h1 className="flex items-center cursor-pointer" onClick={() => navigate('/home')}>
+                            <img src={logoImage} alt="E-Reserve" className="h-12 w-auto" />
+                        </h1>
+                        <nav>
+                            <ul className="flex space-x-10">
+                                <li>
+                                    <Link to="/home" className="hover:text-yellow-300 transition">Accueil</Link>
+                                </li>
+                                <li>
+                                    <Link to="/home" className="hover:text-yellow-300 transition">Devenir administrateur</Link>
+                                </li>
+                                <li className="relative group">
+                                    <span className="hover:text-yellow-300 transition cursor-pointer flex items-center">
+                                        Services
+                                        <FaChevronDown className="ml-2 text-sm" style={{ fontWeight: 'normal' }} />
+                                    </span>
+                                    <ul className="absolute left-0 mt-2 w-39 bg-white text-blue-800 shadow-lg rounded-lg opacity-0 group-hover:opacity-100 group-hover:translate-y-1 transition-all duration-300 ease-in-out transform scale-95 group-hover:scale-100">
+                                        <li className="hover:bg-blue-600 hover:text-white transition rounded-t-lg">
+                                            <button onClick={() => scrollToSection('carousel')} className="block px-4 py-2 text-left w-full">Événement</button>
+                                        </li>
+                                        <li className="hover:bg-blue-600 hover:text-white transition">
+                                            <button onClick={() => scrollToSection('chambre')} className="block px-4 py-2 text-left w-full">Chambre</button>
+                                        </li>
+                                        <li className="hover:bg-blue-600 hover:text-white transition rounded-b-lg">
+                                            <button onClick={() => scrollToSection('reserve')} className="block px-4 py-2 text-left w-full">Transport</button>
+                                        </li>
+                                    </ul>
+                                </li>
+                                <li>
+                                    <span
+                                        onClick={() => navigate('/login')}
+                                        className="bg-yellow-400 text-blue-800 font-semibold py-3 px-8 rounded-full hover:bg-blue-800 hover:text-white transition duration-300 cursor-pointer"
+                                    >
+                                        Se connecter
+                                    </span>
+                                </li>
+                            </ul>
+                        </nav>
+                    </div>
+                </header>
+
+                <div id="carousel">
+                    <Carousel />
                 </div>
-            </header>
 
-            <div id="carousel">
-                <Carousel />
-            </div>
+                <div id="chambre">
+                </div>
+{/* 
+                <div id="reserve">
+                    <Reserve />
+                </div> */}
 
-            <div id="chambre">
-            </div>
+                <TicketSteps />
 
-            <div id="reserve">
-                <Reserve />
-            </div>
-
-            <TicketSteps />
-
-            <div className="flex justify-center items-center py-10">
-                <div className="w-full max-w-4xl p-5 bg-white rounded-lg shadow-lg">
-                    <div className="flex flex-col lg:flex-row items-center lg:items-start">
-                        <div className="lg:w-1/2 p-5">
-                            <img src={TicketStepsImage} alt="TicketSteps" className="w-full h-auto rounded-lg shadow-lg" />
-                        </div>
-                        <div className="lg:w-1/2 p-5 flex flex-col justify-center">
-                            <h2 className="text-3xl font-bold mb-4 text-center lg:text-left">Gérez facilement vos services de réservations</h2>
-                            <p className="text-gray-600 mb-6 text-center lg:text-left">
-                                Ticketplace est une plateforme 100% en ligne de gestion d’événements incluant : Billetterie en ligne et au guichet, inscriptions, invitations, contrôle d’accès, finance, marketing, etc... Elle permet l’administration à 360° et la supervision en temps réel de votre événement.
-                            </p>
-                            <button className="bg-[#0A5DA6] text-white font-semibold py-3 px-6 rounded-lg hover:bg-[#1f405d] transition duration-300 mx-auto lg:mx-0">
-                                Créer un événement
-                            </button>
+                <div className="flex justify-center items-center py-10">
+                    <div className="w-full max-w-4xl p-5 bg-white rounded-lg shadow-lg">
+                        <div className="flex flex-col lg:flex-row items-center lg:items-start">
+                            <div className="lg:w-1/2 p-5">
+                                <img src={TicketStepsImage} alt="TicketSteps" className="w-full h-auto rounded-lg shadow-lg" />
+                            </div>
+                            <div className="lg:w-1/2 p-5 flex flex-col justify-center">
+                                <h2 className="text-3xl font-bold mb-4 text-center lg:text-left">Gérez facilement vos services de réservations</h2>
+                                <p className="text-gray-600 mb-6 text-center lg:text-left">
+                                    E-reserve est une plateforme 100% en ligne de gestion de service de réservation incluant : Billetterie en ligne, inscriptions, invitations, contrôle d’accès, marketing, etc... Elle permet l’administration et la supervision en temps réel de votre événement.
+                                </p>
+                                <button className="bg-[#0A5DA6] text-white font-semibold py-3 px-6 rounded-lg hover:bg-[#1f405d] transition duration-300 mx-auto lg:mx-0">
+                                    Créer un événement
+                                </button>
+                            </div>
                         </div>
                     </div>
                 </div>
-            </div>
 
 
-            <div className="pt-32">
-                <Footer />
+                <div className="pt-32">
+                    <Footer />
+                </div>
             </div>
         </>
     );
--- FrontendSiteVitrine/src/sections/SiteVitrine1/TicketSteps.jsx
+++ FrontendSiteVitrine/src/sections/SiteVitrine1/TicketSteps.jsx
@@ -4,34 +4,37 @@ import '@fortawesome/fontawesome-free/css/all.min.css';
 
 const TicketSteps = () => {
     return (
-        <div className="relative min-h-72 bg-fixed bg-cover bg-center text-white" style={{ backgroundImage: `url(${backgroundImage})` }}>
-            <div className="relative z-10 py-12" style={{ fontFamily: 'Poppins, sans-serif' }}>
-                <div className="text-center text-2xl font-bold mb-8">COMMENT ACHETER VOTRE TICKET ?</div>
-                <div className="flex justify-center space-x-48">
-                    <div className="flex flex-col items-center group">
-                        <div className="bg-gray-900 bg-opacity-70 p-6 rounded-full transition-transform transform group-hover:scale-110">
-                            <i className="fas fa-calendar-alt text-yellow-500 text-5xl"></i>
+        <>
+            
+                <div className="relative min-h-72 bg-fixed bg-cover bg-center text-gray-300" style={{ backgroundImage: `url(${backgroundImage})` }}>
+                    <div className="relative z-10 py-12" style={{ fontFamily: 'Poppins, sans-serif' }}>
+                        <div className="text-center text-2xl font-bold mb-8">COMMENT ACHETER VOTRE TICKET ?</div>
+                        <div className="flex justify-center space-x-52">
+                            <div className="flex flex-col items-center group">
+                                <div className="bg-gray-900 bg-opacity-70 p-6 rounded-full transition-transform transform group-hover:scale-110">
+                                    <i className="fas fa-calendar-alt text-yellow-500 text-5xl"></i>
+                                </div>
+                                <div className="mt-4">Réservation de billets</div>
+                            </div>
+                            <div className="flex flex-col items-center group">
+                                <div className="bg-gray-900 bg-opacity-70 p-6 rounded-full transition-transform transform group-hover:scale-110">
+                                    <i className="fas fa-check-square text-yellow-500 text-5xl"></i>
+                                </div>
+                                <div className="mt-4">Confirmation de la commande</div>
+                            </div>
+                            <div className="flex flex-col items-center group">
+                                <div className="bg-gray-900 bg-opacity-70 p-6 rounded-full transition-transform transform group-hover:scale-110">
+                                    <i className="fas fa-credit-card text-yellow-500 text-5xl"></i>
+                                </div>
+                                <div className="mt-4">Paiement</div>
+                            </div>
                         </div>
-                        <div className="mt-4">Réservation de billets</div>
                     </div>
-                    <div className="flex flex-col items-center group">
-                        <div className="bg-gray-900 bg-opacity-70 p-6 rounded-full transition-transform transform group-hover:scale-110">
-                            <i className="fas fa-check-square text-yellow-500 text-5xl"></i>
-                        </div>
-                        <div className="mt-4">Confirmation de la commande</div>
-                    </div>
-                    <div className="flex flex-col items-center group">
-                        <div className="bg-gray-900 bg-opacity-70 p-6 rounded-full transition-transform transform group-hover:scale-110">
-                            <i className="fas fa-credit-card text-yellow-500 text-5xl"></i>
-                        </div>
-                        <div className="mt-4">Paiement</div>
-                    </div>
-                </div>
-            </div>
 
-            {/* Couche de fond sombre */}
-            <div className="absolute inset-0 bg-slate-950 opacity-50 z-0"></div>
-        </div>
+                    {/* Couche de fond sombre */}
+                    <div className="absolute inset-0 bg-slate-950 opacity-50 z-0"></div>
+                </div>
+        </>
     );
 };
 
