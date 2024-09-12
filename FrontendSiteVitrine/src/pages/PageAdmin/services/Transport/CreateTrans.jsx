3847827c0b1cb24144d9fc23e1f912bd1c54f7fb
Lilasss <razafindramboahantasoa@gmail.com>
2024-09-12 20:48:42 +0300

    seat 1

--- FrontendSiteVitrine/src/pages/PageAdmin/services/Transport/CreateTrans.jsx
+++ FrontendSiteVitrine/src/pages/PageAdmin/services/Transport/CreateTrans.jsx
@@ -1,13 +1,42 @@
-import React from 'react'
-import TransportReservationForm from './TransportReservationForm'
+import React, { useState } from 'react';
+import TransportReservationForm from './TransportReservationForm';
+import SeatSelection from './SeatSelection';
 
-function CreateTrans() {
+const CreateTrans = () => {
+  const [step, setStep] = useState(1); // État pour gérer l'étape
+  const [formData, setFormData] = useState({
+    categorie: "",
+    date_depart: new Date(),
+    heure_depart: "08:00",
+    image_path: "",
+    lieu_arriver: "",
+    lieu_depart: "",
+    name: "",
+    nombre_place: 1,
+    prix: "",
+  });
+
+  // Fonction pour aller à l'étape suivante
+  const nextStep = () => {
+    setStep(step + 1);
+  };
+
+  // Fonction pour revenir à l'étape précédente
+  const prevStep = () => {
+    setStep(step - 1);
+  };
+
+  // Rendu conditionnel selon l'étape
   return (
-    <>
-      <TransportReservationForm />
-    </>
+    <div>
+      {step === 1 && (
+        <TransportReservationForm formData={formData} setFormData={setFormData} nextStep={nextStep} />
+      )}
+      {step === 2 && (
+        <SeatSelection formData={formData} prevStep={prevStep} />
+      )}
+    </div>
+  );
+};
 
-  )
-}
-
-export default CreateTrans
+export default CreateTrans;
